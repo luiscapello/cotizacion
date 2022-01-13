21fd5b511a3c2d43b40238ab27082f155aa5de95
luiscapello <capello3005@gmail.com>
2022-01-13 17:14:15 -0600

    Create acciones.py
    metodo crear

---
+++ cotizacion/notas/acciones.py
@@ -0,0 +1,20 @@
+import notas.nota as modelo
+
+
+class Acciones:
+
+    def crear(self, usuario):
+        print(f"\nOk {usuario[2]}!! Vamos a crear una nota...")
+
+        titulo = input("introduce el titulo de tu nota: ")
+        descripcion = input("Introduce el contennido de la nota: ")
+
+        nota = modelo.Notas(usuario[0], titulo, descripcion)
+        guardar = nota.guardar()
+
+        if guardar[0] >= 1:
+            print(f"\nPerfecto has guardado la nota: {nota.titulo}")
+
+        else:
+            print(f"\nNo se ha guardado la nota {usuario[2]} ")
+
