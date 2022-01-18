86b663442b10be68892945b01e101a9b382734a0
luiscapello <capello3005@gmail.com>
2022-01-18 16:05:12 -0600

    Update nota.py

--- cotizacion/notas/nota.py
+++ cotizacion/notas/nota.py
@@ -7,7 +7,7 @@ cursor = connect[1]
 
 class Nota:
 
-    def __init__(self, usuario_id, titulo, descripcion):
+    def __init__(self, usuario_id, titulo = "", descripcion = ""):
         self.usuario_id = usuario_id
         self.titulo = titulo
         self.descripcion = descripcion
@@ -20,3 +20,11 @@ class Nota:
         database.commit()
 
         return[cursor.rowcount, self]
+
+    def listar(self):
+        sql = f"SELECT * FROM notas WHERE usuario_id = {self.usuario_id}"
+
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        return result
