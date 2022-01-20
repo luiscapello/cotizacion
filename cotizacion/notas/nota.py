f5a7bad81b3130898342552916a73a08945979cf
luiscapello <capello3005@gmail.com>
2022-01-20 13:46:54 -0600

    Update nota.py

--- cotizacion/notas/nota.py
+++ cotizacion/notas/nota.py
@@ -7,7 +7,7 @@ cursor = connect[1]
 
 class Nota:
 
-    def __init__(self, usuario_id, titulo = "", descripcion = ""):
+    def __init__(self, usuario_id, titulo , descripcion):
         self.usuario_id = usuario_id
         self.titulo = titulo
         self.descripcion = descripcion
